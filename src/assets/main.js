f8f1e9050b3487ffa1409d667c80de23c484696b
Yannick Lapp <yannick.lapp@cn-consult.eu>
2017-10-16 09:33:40 +0200

    Add badges to output
    Added an ajax call which retrieves and displays the badges on the website.

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,23 @@
-$(function() {
+$(document).ready(function() {
 
   // your code will go here
+  $.ajax(
+    {
+      url: "https://www.codeschool.com/users/ylapp.json",
+      dataType: "jsonp",
+      success: function(result)
+      {
 
+        $.each(result.courses.completed, function(index, entry)
+        {
+          var div = $("<div></div>").addClass("course");
+
+          div.append("<h3>" + entry.title + "</h3>");
+          div.append("<img src='" + entry.badge + "'>");
+          div.append("<a href='" + entry.url + "' target='blank' class='btn btn-primary'>See course</a>");
+          $("#badges").append(div);
+        });
+
+      }
+    });
 });
